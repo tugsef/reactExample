68c0ef13983eb0d4f722fdbba35de43b13d122e7
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2023-04-10 07:20:48 +0300

    add todoApp
    The HTML page has been converted to a react project.

---
+++ my-todoApp/src/components/Todo/index.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+<section className="todoapp">
+	<header className="header">
+		<h1>todos</h1>
+		<form>
+			<input className="new-todo" placeholder="What needs to be done?" autoFocus />
+		</form>
+	</header>
+	
+	<section className="main">
+		<input className="toggle-all" type="checkbox" />
+		<label htmlFor="toggle-all">
+			Mark all as complete
+		</label>
+
+		<ul className="todo-list">
+			<li className="completed">
+				<div className="view">
+					<input className="toggle" type="checkbox" />
+					<label>Learn JavaScript</label>
+					<button className="destroy"></button>
+				</div>
+			</li>
+			<li>
+				<div className="view">
+					<input className="toggle" type="checkbox" />
+					<label>Learn React</label>
+					<button className="destroy"></button>
+				</div>
+			</li>
+			<li>
+				<div className="view">
+					<input className="toggle" type="checkbox" />
+					<label>Have a life!</label>
+					<button className="destroy"></button>
+				</div>
+			</li>
+		</ul>
+	</section>
+
+	<footer className="footer">
+		<span className="todo-count">
+			<strong>2</strong>
+			items left
+		</span>
+
+		<ul className="filters">
+			<li>
+				<a href="#/" className="selected">All</a>
+			</li>
+			<li>
+				<a href="#/">Active</a>
+			</li>
+			<li>
+				<a href="#/">Completed</a>
+			</li>
+		</ul>
+
+		<button className="clear-completed">
+			Clear completed
+		</button>
+	</footer>
+</section>
+
+<footer className="info">
+	<p>Click to edit a todo</p>
+	<p>Created by <a href="https://d12n.me/">Dmitry Sharabin</a></p>
+	<p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
+</footer>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Todo
---
+++ my-todoApp/src/components/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from './Todo'
+import "./styles.css"
+function Components() {
+  return (
+    <div>
+        <Todo />
+    </div>
+  )
+}
+
+export default Components
