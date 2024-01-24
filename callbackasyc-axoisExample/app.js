8fed607f1e8711160f68daff9b26f5c45865ad97
Sefa Demirtaş <39422788+tugsef@users.noreply.github.com>
2024-01-24 08:46:48 +0300

    Update app.js

--- callbackasyc-axoisExample/app.js
+++ callbackasyc-axoisExample/app.js
@@ -1,14 +1,17 @@
-import axios from "axios";
+import axios from "axios"
 
+const getApi = async (userId) => {
 
-const getAl = async (user_id) => {
-    const {data:user} = await axios("https://jsonplaceholder.typicode.com/users/" + user_id);
-    const {data:post} = await axios("https://jsonplaceholder.typicode.com/posts?userId=" + user_id);
+    try {
+        const { data: user } = await axios(`https://jsonplaceholder.typicode.com/users/${userId}`)
+        const { data: post } = await axios(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
 
-    console.log(user);
-    console.log(post);
+        return { user, post }
 
+    } catch (error) {
+        console.log(error);
+    }
 
 }
 
-export default getAl;
+export { getApi }
